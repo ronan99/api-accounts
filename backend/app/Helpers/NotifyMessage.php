a7f2bb1ddbcb5955c5e69dae6055a4eb93263060
ronan99 <ronan.lino99@gmail.com>
2024-03-04 23:48:57 -0400

    Correção de PSR

--- backend/app/Helpers/NotifyMessage.php
+++ backend/app/Helpers/NotifyMessage.php
@@ -6,18 +6,15 @@ use App\Helpers\Currency\CurrencyFactory;
 
 class NotifyMessage {
 
-    protected $formatter;
-    public function __construct(){
-        $this->formatter = CurrencyFactory::getFormatter("BRL");
-    }
     public function failedTransaction($user, $amount){
-
-        $realAmount = $this->formatter->format($amount);
+        $formatter = CurrencyFactory::getFormatter("BRL");
+        $realAmount = $formatter->format($amount);
         return "A transferência para o usuário {$user} falhou e o valor de {$realAmount} foi retornado para sua conta.";
     }
 
     public function successTransaction($userFromName, $userToName, $amount){
-        $realAmount = $this->formatter->format($amount);
+        $formatter = CurrencyFactory::getFormatter("BRL");
+        $realAmount = $formatter->format($amount);
         return "Olá, {$userToName}, Você recebeu uma transferência de {$userFromName} no valor de {$realAmount}";
     }
 }
